9f5cbf82dd4f05dea3cd8ffa37669a85bb394df5
NFlorenceNdou <80393494+NFlorenceNdou@users.noreply.github.com>
2021-11-11 15:22:46 +0100

    Update swap-newsletter-placement.js

--- swap-newsletter-placement.js
+++ swap-newsletter-placement.js
@@ -1,6 +1,8 @@
 const div1 = document.querySelector("body > main > main > div");
 const div2 = document.querySelector("body > footer > div.footer-top > div > div > div.footer-newsletter.col-sm-12");
+const div3 = document.querySelector("body > footer > div.footer-top > div > div > div.footer-newsletter.col-sm-12 > zone")
 div1.after(div2)
 div2.style['text-align'] = 'center';
 div2.style['padding-bottom'] = '20px';
 div2.style['float'] = 'right';
+
