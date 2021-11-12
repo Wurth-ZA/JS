b8e938aa61bb631b48f94e696feaafae4018d6e6
NFlorenceNdou <80393494+NFlorenceNdou@users.noreply.github.com>
2021-11-12 10:24:24 +0100

    subscribe form changes

--- subscribe-form.js
+++ subscribe-form.js
@@ -1,23 +1,30 @@
-const form = document.createElement("form");
-form.setAttribute('action', "/wurth/newsletter/subscribe");
-form.setAttribute('method', "post");
-form.setAttribute('class', "contenteddietor-newslettersubscribe form-horizontal");
-
-const email = document.createElement("input");
-email.setAttribute('type', "text");
-email.setAttribute('name', "email");
-email.setAttribute('data-item', "email");
-email.setAttribute('placeholder', "Email Address");
-
-const submit = document.createElement("input");
-submit.setAttribute('type', "submit");
-submit.setAttribute('value', "submit");
-
-form.appendChild(email);
-form.appendChild(submit);
-var form_medium = document.querySelector("body > main > main > div > div > div > div");
-
-if (form_medium) {
-    form_medium.parentElement.insertBefore(form, form_medium.nextSibling)
-}
+            const form = document.createElement("form");
+            form.setAttribute('action', "/wurth/newsletter/subscribe");
+            form.setAttribute('method', "post");
+            form.setAttribute('class', "contenteddietor-newslettersubscribe form-horizontal");
+            
+            const email = document.createElement("input");
+            email.setAttribute('type', "text");
+            email.setAttribute('name', "email");
+            email.setAttribute('data-item', "email");
+            email.setAttribute('placeholder', "Email Address");
+            email.setAttribute('class', "form-control newsletter-control");
+            email.setAttribute('placeholder', "Email Address");
+            email.setAttribute('autocomplete', "off");
+            email.setAttribute('data-validate', "true");
+            email.setAttribute('data-validate-required', "true");
+            email.setAttribute('data-validate-email', "true");
+            
+            const submit = document.createElement("input");
+            submit.setAttribute('type', "submit");
+            submit.setAttribute('value', "submit");
+            submit.setAttribute('class', "btn")
+            
+            form.appendChild(email);
+            form.appendChild(submit);
+            var form_medium = document.querySelector("body > main > main > div > div > div > div");
+            
+            if (form_medium) {
+                form_medium.parentElement.insertBefore(form, form_medium.nextSibling)
+            }
 //document.body.appendChild(form);
